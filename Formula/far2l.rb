79e91efbb565f1f1999a455e151e53065d69b182
Yurii Kolesnykov <root@yurikoles.com>
2018-11-07 15:04:36 +0200

    far2l: add stable version spec

--- Formula/far2l.rb
+++ Formula/far2l.rb
@@ -1,10 +1,12 @@
 class Far2l < Formula
   desc "Linux port of FAR v2"
   homepage "https://github.com/elfmz/far2l"
+  url "https://github.com/elfmz/far2l/archive/alpha-02nov18.tar.gz"
+  version "0.2-alpha"
+  sha256 "971d170698d9567d28c547ba5d49234f6d0f9e5b1dece0ab366037daea87e675"
   head "https://github.com/elfmz/far2l.git"
-  depends_on "cmake" => :build
 
-  depends_on "cmake"
+  depends_on "cmake" => :build
   depends_on "gawk"
   depends_on "glib"
   depends_on "pkg-config"
@@ -15,6 +17,9 @@ class Far2l < Formula
     system "cmake", ".", *std_cmake_args
     system "make", "install", "DESTDIR=#{prefix}"
     mkdir_p bin.to_s
-    ln_sf "#{prefix}/usr/bin/far2l", bin.to_s
+    (bin/"far2l").write <<~EOS
+      #!/usr/bin/env bash
+      "#{prefix}/usr/bin/far2l"
+    EOS
   end
 end
