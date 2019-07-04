d9486b5aba11866b72fb099357462c3058f1d65d
Yurii Kolesnykov <root@yurikoles.com>
2019-07-04 23:43:54 +0300

    update svt-av1
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/svt-av1.rb
+++ Formula/svt-av1.rb
@@ -3,6 +3,7 @@ class SvtAv1 < Formula
   homepage "https://github.com/OpenVisualCloud/SVT-AV1"
   url "https://github.com/OpenVisualCloud/SVT-AV1/archive/v0.6.0.tar.gz"
   sha256 "4ff6f05a9528fe8a7b3d6708fe1115d15d26a5a9b0b676d8a250a0b2e8630579"
+
   depends_on "cmake" => :build
 
   def install
