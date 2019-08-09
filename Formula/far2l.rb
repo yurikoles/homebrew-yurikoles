bdda15f5c363099c77d682311d00aff5225fb854
Yurii Kolesnykov <root@yurikoles.com>
2019-08-09 12:12:49 +0300

    far2l 0.2-alpha.2019-07-23
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/far2l.rb
+++ Formula/far2l.rb
@@ -1,25 +1,27 @@
 class Far2l < Formula
   desc "Linux port of FAR v2"
   homepage "https://github.com/elfmz/far2l"
-  url "https://github.com/elfmz/far2l/archive/alpha-02nov18.tar.gz"
-  version "0.2-alpha"
-  sha256 "971d170698d9567d28c547ba5d49234f6d0f9e5b1dece0ab366037daea87e675"
+  url "https://github.com/elfmz/far2l/archive/alpha-23jun19.tar.gz"
+  version "0.2-alpha.2019-07-23"
+  sha256 "0e9a3a25f0954a201d210baab12c14a496df8e49c6d3188d921ba4ff50b72800"
   head "https://github.com/elfmz/far2l.git"
 
   depends_on "cmake" => :build
   depends_on "gawk"
   depends_on "glib"
+  depends_on "neon"
   depends_on "pkg-config"
   depends_on "wget"
   depends_on "wxmac"
 
   def install
     system "cmake", ".", *std_cmake_args
-    system "make", "install", "DESTDIR=#{prefix}"
+    system "make", "install"
+    mv bin/"far2l", bin/"far2l.orig"
     mkdir_p bin.to_s
     (bin/"far2l").write <<~EOS
-      #!/usr/bin/env bash
-      "#{prefix}/usr/bin/far2l"
+      #!/bin/sh
+      "#{prefix}/bin/far2l.orig"
     EOS
   end
 end
