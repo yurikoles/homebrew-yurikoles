4366365f35fda60ae453f62526b832720f929211
Yurii Kolesnykov <root@yurikoles.com>
2019-10-01 22:13:59 +0300

    SVT-AV1 0.7.0
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/svt-av1.rb
+++ Formula/svt-av1.rb
@@ -1,14 +1,18 @@
 class SvtAv1 < Formula
   desc "Scalable Video Technology for AV1 Encoder"
   homepage "https://github.com/OpenVisualCloud/SVT-AV1"
-  url "https://github.com/OpenVisualCloud/SVT-AV1/archive/v0.6.0.tar.gz"
-  sha256 "4ff6f05a9528fe8a7b3d6708fe1115d15d26a5a9b0b676d8a250a0b2e8630579"
+  url "https://github.com/OpenVisualCloud/SVT-AV1/archive/v0.7.0.tar.gz"
+  sha256 "69893ec8b8515dd3bcde78ff955a908537acd52f22310f0f188f4e98b239000a"
 
   depends_on "cmake" => :build
+  depends_on "ninja" => :build
 
   def install
-    system "cmake", ".", *std_cmake_args
-    system "make", "install"
+    mkdir "build" do
+      system "cmake", "..", "-G", "Ninja", *std_cmake_args
+      system "ninja"
+      system "ninja", "install"
+    end
   end
 
   test do
