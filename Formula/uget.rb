ad25e3c2282cf85a89d51bfc5c9a4e3677b140c7
Yurii Kolesnykov <root@yurikoles.com>
2020-02-21 20:18:31 +0200

    new formula: uget 2.2.3
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

---
+++ Formula/uget.rb
@@ -0,0 +1,36 @@
+class Uget < Formula
+  desc "#1 Open Source Download Manager"
+  homepage "https://ugetdm.com/"
+  url "https://sourceforge.net/projects/urlget/files/uget%20%28stable%29/2.2.3/uget-2.2.3-1.tar.gz"
+  sha256 "11356e4242151b9014fa6209c1f0360b699b72ef8ab47dbeb81cc23be7db9049"
+
+  head do
+    url "https://git.code.sf.net/p/urlget/uget2.git"
+
+    depends_on "autoconf" => :build
+    depends_on "automake" => :build
+    depends_on "gettext" => :build
+  end
+
+  depends_on "intltool" => :build
+  depends_on "pkg-config" => :build
+  depends_on "aria2"
+  depends_on "gstreamer"
+  depends_on "gtk+3"
+  depends_on "libnotify"
+  uses_from_macos "curl"
+
+  def install
+    chmod "+x", "./autogen.sh" if build.head?
+    system "./autogen.sh" if build.head?
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
+    system "make", "install"
+  end
+
+  test do
+    assert `"#{bin}/uget-gtk" --help | grep -- --help` =~ /--help/
+  end
+end
