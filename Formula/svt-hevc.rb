d22417c2c7ba59d6b8014acf924661240d70c290
Yurii Kolesnykov <root@yurikoles.com>
2020-08-07 16:04:56 +0300

    svt-hevc 1.5.0_1: * add yasm build dep * port test from homebrew/core PR: https://git.io/JJPSk
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/svt-hevc.rb
+++ Formula/svt-hevc.rb
@@ -4,8 +4,15 @@ class SvtHevc < Formula
   url "https://github.com/OpenVisualCloud/SVT-HEVC/archive/v1.5.0.tar.gz"
   sha256 "5331213ce3fe73d5fe2887cf3aabcd8ffde2fafd0e5f09928d412dc8306f3992"
   head "https://github.com/OpenVisualCloud/SVT-HEVC.git"
+  revision 1
 
   depends_on "cmake" => :build
+  depends_on "yasm" => :build
+
+  resource "akiyo_qcif.y4m" do
+    url "https://media.xiph.org/video/derf/y4m/akiyo_qcif.y4m"
+    sha256 "df88d83cbf6d99f3ec41f2c1fd2e67665d2a71ff8caa08f8b6bc46bf4567ea2e"
+  end
 
   def install
     mkdir "build" do
@@ -15,6 +22,16 @@ class SvtHevc < Formula
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/SvtHevcEncApp --version")
+    resource("akiyo_qcif.y4m").stage testpath
+    # Create an example mp4 file
+    mp4out = testpath/"video.mp4"
+    (testpath/"command.sh").write <<~EOS
+      #!/bin/sh
+      ulimit -n 512
+      #{bin}/SvtHevcEncApp -i #{testpath}/akiyo_qcif.y4m -b #{mp4out}
+    EOS
+    chmod 0555, testpath/"command.sh"
+    system "./command.sh"
+    assert_predicate mp4out, :exist?
   end
 end
