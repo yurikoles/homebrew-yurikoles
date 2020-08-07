7448db38ddb268dc86958487762c7c67d2d3ec7f
Yurii Kolesnykov <root@yurikoles.com>
2020-08-07 15:56:24 +0300

    svt-hevc 1.5.0
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/svt-hevc.rb
+++ Formula/svt-hevc.rb
@@ -1,8 +1,8 @@
 class SvtHevc < Formula
   desc "Scalable Video Technology for HEVC Encoder"
   homepage "https://01.org/svt"
-  url "https://github.com/OpenVisualCloud/SVT-HEVC/archive/v1.4.3.tar.gz"
-  sha256 "08bf2b1075609788194bf983d693ab38ed30214ec966afa67895209fd0bf2179"
+  url "https://github.com/OpenVisualCloud/SVT-HEVC/archive/v1.5.0.tar.gz"
+  sha256 "5331213ce3fe73d5fe2887cf3aabcd8ffde2fafd0e5f09928d412dc8306f3992"
   head "https://github.com/OpenVisualCloud/SVT-HEVC.git"
 
   depends_on "cmake" => :build
