922ceec6120604464c0d95d6eee7bd96097241f3
Yurii Kolesnykov <root@yurikoles.com>
2020-09-24 16:49:43 +0300

    ocbuilder 7.5.3
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/ocbuilder.rb
+++ Casks/ocbuilder.rb
@@ -1,6 +1,6 @@
 cask "ocbuilder" do
-  version "7.4.4"
-  sha256 "fb8efed1ad44ec9c0e7020e4b4dc29725b5808baf3848f5eb78b683b36ab69bd"
+  version "7.5.3"
+  sha256 "804813bfaba06cc0175a137583d6e70dff97aa985608d598a1d711971ccbaf71"
 
   url "https://github.com/Pavo-IM/ocbuilder/releases/download/#{version}/OCBuilder.app.zip"
   appcast "https://github.com/Pavo-IM/ocbuilder/releases.atom"
