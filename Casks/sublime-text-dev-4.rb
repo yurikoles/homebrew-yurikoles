a754254ed193762f48cef664963b7236baefceb4
Yurii Kolesnykov <root@yurikoles.com>
2020-09-25 12:31:40 +0300

    sublime-text-dev-4 4.087
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/sublime-text-dev-4.rb
+++ Casks/sublime-text-dev-4.rb
@@ -1,6 +1,6 @@
 cask "sublime-text-dev-4" do
-  version "4.086"
-  sha256 "87b562dd35e63ba16523ef5b1f4e8fb755079bcb5a71b6689aeaf894fbfc3dab"
+  version "4.087"
+  sha256 "e182dadb3646927962cf41753d6d9b0854566a880320047a03564e6e3fad6a31"
 
   url "https://download.sublimetext.com/sublime_text_build_#{version.no_dots}_mac.zip"
   appcast "https://www.sublimetext.com/updates/#{version.major}/dev_update_check",
