cdf2552388354a80c78cce283dad733b8a6973b5
Yurii Kolesnykov <root@yurikoles.com>
2020-10-02 20:48:24 +0300

    deadbeef: lint
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/deadbeef.rb
+++ Casks/deadbeef.rb
@@ -1,10 +1,11 @@
-cask 'deadbeef' do
+cask "deadbeef" do
   version :latest
   sha256 :no_check
 
-  url 'https://downloads.sourceforge.net/deadbeef/travis/osx/master/deadbeef-devel-osx-x86_64.zip'
-  name 'DeaDBeeF'
-  homepage 'http://deadbeef.sourceforge.net/'
+  url "https://downloads.sourceforge.net/deadbeef/travis/osx/master/deadbeef-devel-osx-x86_64.zip"
+  name "DeaDBeeF"
+  desc "Modular audio player for UNIX-like systems"
+  homepage "https://deadbeef.sourceforge.io/"
 
-  app 'deadbeef.app'
+  app "deadbeef.app"
 end
