0efbcc59ca2ae788a8d82298ace91f89db5b7dda
Yurii Kolesnykov <root@yurikoles.com>
2020-10-02 20:28:47 +0300

    virtualbox-snapshot: minor fixes
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/virtualbox-snapshot.rb
+++ Casks/virtualbox-snapshot.rb
@@ -1,45 +1,44 @@
-cask 'virtualbox-snapshot' do
+cask "virtualbox-snapshot" do
   version :latest
   sha256 :no_check
 
   url do
-    require 'open-uri'
-    downloads_url = 'https://www.virtualbox.org/wiki/Testbuilds'
-    versions = URI(downloads_url.to_s)
-               .open
-               .read
-               .scan(%r{href=".*(VirtualBox-6.1.1+\-\d+\-OSX.dmg)"})
-               .flatten
-    "https://www.virtualbox.org/download/testcase/#{versions.last}"
+    require "open-uri"
+    downloads_url = "https://www.virtualbox.org/wiki/Testbuilds"
+    version = URI(downloads_url.to_s)
+              .open.read
+              .scan(/href=".*(VirtualBox-\d+.\d+.+\d+-\d+-OSX.dmg)"/)
+              .flatten.last
+    "https://www.virtualbox.org/download/testcase/#{version}"
   end
-  name 'Oracle VirtualBox Development Snapshot'
-  homepage 'https://www.virtualbox.org/wiki/Testbuilds'
+  name "Oracle VirtualBox Development Snapshot"
+  homepage "https://www.virtualbox.org/wiki/Testbuilds"
 
   conflicts_with cask: [
-                         'virtualbox',
-                         'virtualbox-beta',
-                       ]
+    "virtualbox",
+    "virtualbox-beta",
+  ]
 
-  pkg 'VirtualBox.pkg'
+  pkg "VirtualBox.pkg"
 
   uninstall script:  {
-                       executable: 'VirtualBox_Uninstall.tool',
-                       args:       ['--unattended'],
-                       sudo:       true,
-                     },
-            pkgutil: 'org.virtualbox.pkg.*'
+    executable: "VirtualBox_Uninstall.tool",
+    args:       ["--unattended"],
+    sudo:       true,
+  },
+            pkgutil: "org.virtualbox.pkg.*"
 
   zap trash: [
-               '/Library/Application Support/VirtualBox',
-               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.virtualbox.app.virtualbox.sfl*',
-               '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.virtualbox.app.virtualboxvm.sfl*',
-               '~/Library/VirtualBox',
-               '~/Library/Preferences/org.virtualbox.app.VirtualBox.plist',
-               '~/Library/Preferences/org.virtualbox.app.VirtualBoxVM.plist',
-               '~/Library/Saved Application State/org.virtualbox.app.VirtualBox.savedState',
-               '~/Library/Saved Application State/org.virtualbox.app.VirtualBoxVM.savedState',
-             ],
-      rmdir: '~/VirtualBox VMs'
+    "/Library/Application Support/VirtualBox",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.virtualbox.app.virtualbox.sfl*",
+    "~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/org.virtualbox.app.virtualboxvm.sfl*",
+    "~/Library/VirtualBox",
+    "~/Library/Preferences/org.virtualbox.app.VirtualBox.plist",
+    "~/Library/Preferences/org.virtualbox.app.VirtualBoxVM.plist",
+    "~/Library/Saved Application State/org.virtualbox.app.VirtualBox.savedState",
+    "~/Library/Saved Application State/org.virtualbox.app.VirtualBoxVM.savedState",
+  ],
+      rmdir: "~/VirtualBox VMs"
 
   caveats do
     kext
