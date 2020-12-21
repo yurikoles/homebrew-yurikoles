84caaf2934a2469197ce566ec441260849417bd5
Yurii Kolesnykov <root@yurikoles.com>
2020-12-21 14:15:18 +0200

    sublime-text-dev-4 4.094
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/sublime-text-dev-4.rb
+++ Casks/sublime-text-dev-4.rb
@@ -1,12 +1,13 @@
 cask "sublime-text-dev-4" do
-  version "4.093"
-  sha256 "c42114ff4a831b2e94917dca5f5ba0cefc99b2d3f632e64871924c8784a4e694"
+  version "4.094"
+  sha256 "7a7e3f8c391d783df4144e1ad71c50778f77e350ac4f5b095da38d7aecac4267"
 
   url "https://download.sublimetext.com/sublime_text_build_#{version.no_dots}_mac.zip"
   appcast "https://www.sublimetext.com/updates/#{version.major}/dev_update_check",
-          configuration: version.no_dots
+          must_contain: version.no_dots
   name "Sublime Text"
-  homepage "https://www.sublimetext.com/dev"
+  desc "Text editor for code, markup and prose"
+  homepage "https://www.sublimetext.com/"
 
   auto_updates true
   conflicts_with cask: [
