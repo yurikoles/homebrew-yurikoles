c0501bc5fdfae2a1f717ecd4885f24b6d132ef16
Yurii Kolesnykov <root@yurikoles.com>
2021-01-16 11:09:54 +0200

    ocbuilder 7.5.6
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/ocbuilder.rb
+++ Casks/ocbuilder.rb
@@ -1,10 +1,11 @@
 cask "ocbuilder" do
-  version "7.5.5"
-  sha256 "53d17a929dd8239cd46e00fecadf18a6ec425f27912dccd29969acfd21357bd7"
+  version "7.5.6"
+  sha256 "23959b1d26eb475f14f6e54a00067c578acd75ad0d2ee6015483b55d56491005"
 
   url "https://github.com/Pavo-IM/ocbuilder/releases/download/#{version}/OCBuilder.app.zip"
   appcast "https://github.com/Pavo-IM/ocbuilder/releases.atom"
   name "OCBuilder"
+  desc "Compile OpenCore, common drivers and kexts"
   homepage "https://github.com/Pavo-IM/ocbuilder"
 
   auto_updates true
