0d47e517d7d1a8175344ce144c63084e198fa8b0
Yurii Kolesnykov <root@yurikoles.com>
2021-04-05 20:08:21 +0300

    Update ocbuilder from 7.6.0 to 7.6.1 (#16)

--- Casks/ocbuilder.rb
+++ Casks/ocbuilder.rb
@@ -1,6 +1,6 @@
 cask "ocbuilder" do
-  version "7.6.0"
-  sha256 "16a97f1de02f830639847480145f5f838573a4d57c60ec728d3fe67932e0dde3"
+  version "7.6.1"
+  sha256 "866d416916d6eda6c129a80e3a5bcb9031cb6ad40b2f5ed682a8b773255de3e9"
 
   url "https://github.com/Pavo-IM/ocbuilder/releases/download/#{version}/OCBuilder.app.zip"
   appcast "https://github.com/Pavo-IM/ocbuilder/releases.atom"
