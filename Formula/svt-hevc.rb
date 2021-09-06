de2155ac471a00146455e6a7849fa439bf654086
Yurii Kolesnykov <root@yurikoles.com>
2021-09-06 23:14:30 +0300

    svt-hevc 1.5.1
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/svt-hevc.rb
+++ Formula/svt-hevc.rb
@@ -1,12 +1,17 @@
 class SvtHevc < Formula
   desc "Scalable Video Technology for HEVC Encoder"
   homepage "https://01.org/svt"
-  url "https://github.com/OpenVisualCloud/SVT-HEVC/archive/v1.5.0.tar.gz"
-  sha256 "5331213ce3fe73d5fe2887cf3aabcd8ffde2fafd0e5f09928d412dc8306f3992"
-  revision 2
-  head "https://github.com/OpenVisualCloud/SVT-HEVC.git"
+  url "https://github.com/OpenVisualCloud/SVT-HEVC/archive/v1.5.1.tar.gz"
+  sha256 "fcadf3a326e012bdc7d7357b55573b45dfd37bf4a4186c93ed1c4b98fe3f1f6d"
+  head "https://github.com/OpenVisualCloud/SVT-HEVC.git", branch: "master"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
 
   depends_on "cmake" => :build
+  depends_on "ninja" => :build
   depends_on "yasm" => :build
 
   resource "akiyo_qcif.y4m" do
@@ -15,11 +20,12 @@ class SvtHevc < Formula
   end
 
   def install
-    mkdir "build" do
-      system "cmake", "..", *std_cmake_args
-      system "cmake", "--build", "."
-      system "cmake", "--install", "."
-    end
+    args = std_cmake_args
+    args << "-G" << "Ninja"
+
+    system "cmake", "-S", ".", "-B", "build", *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
