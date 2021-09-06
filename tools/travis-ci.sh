a649fb338aff1f7b79cc753d5b629976cc946b90
Yurii Kolesnykov <root@yurikoles.com>
2021-09-06 23:16:34 +0300

    rm travis-ci
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- tools/travis-ci.sh
+++
@@ -1,9 +0,0 @@
-#/usr/bin/env bash -ex
-
-for file in "$@"; do
-    if [[ $file =~ .rb$ ]] ; then
-        brew style $file
-    	brew audit --strict --online --new-formula $file
-    	brew install $file -vd
-    fi
-done
