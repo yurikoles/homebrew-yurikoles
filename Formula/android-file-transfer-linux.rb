7f296cb0c6aa65a03734a1b7d35390281473d9f1
Yurii Kolesnykov <root@yurikoles.com>
2022-01-09 05:17:06 +0200

    android-file-transfer-linux: update install, test
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/android-file-transfer-linux.rb
+++ Formula/android-file-transfer-linux.rb
@@ -3,7 +3,7 @@ class AndroidFileTransferLinux < Formula
   homepage "https://whoozle.github.io/android-file-transfer-linux/"
   url "https://github.com/whoozle/android-file-transfer-linux/archive/v4.2.tar.gz"
   sha256 "cc607d68e8a18273c9b56975a70a0e68fbdf9d5b903b2727a345a605ff48a19f"
-  revision 1
+  revision 2
   head "https://github.com/whoozle/android-file-transfer-linux.git", branch: "master"
 
   livecheck do
@@ -12,6 +12,7 @@ class AndroidFileTransferLinux < Formula
   end
 
   depends_on "cmake" => :build
+  depends_on "ninja" => :build
   depends_on "openssl"
   depends_on "qt@5"
   depends_on "taglib"
@@ -19,15 +20,19 @@ class AndroidFileTransferLinux < Formula
   depends_on "pybind11" => :recommended
 
   def install
-    args = std_cmake_args
-    args << "-DBUILD_FUSE=OFF"
-    mkdir "build" do
-      system "cmake", "..", *args
-      system "make", "install"
-    end
+    args = std_cmake_args + %w[
+      -S .
+      -B build
+      -G Ninja
+      -DBUILD_FUSE=OFF
+    ]
+
+    system "cmake", *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
   end
 
   test do
-    assert_match version.to_s, shell_output("#{bin}/aft-mtp-cli --version")
+    assert_match "help", shell_output("#{bin}/aft-mtp-cli --help 2>&1")
   end
 end
