941988b2ed59c4ca096be3d222f29d0da61b64d1
Yurii Kolesnykov <root@yurikoles.com>
2022-01-10 03:14:01 +0200

    qbittorrent 4.4.0
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

---
+++ Formula/qbittorrent.rb
@@ -0,0 +1,37 @@
+class Qbittorrent < Formula
+  desc "BitTorrent client"
+  homepage "https://www.qbittorrent.org/"
+  url "https://downloads.sourceforge.net/project/qbittorrent/qbittorrent/qbittorrent-4.4.0/qbittorrent-4.4.0.tar.xz"
+  sha256 "6b783a88c7bd567e48cd9f20c67b788776ee2a6d474fe3df4af216acbdfe501b"
+  license "GPL-2.0-or-later"
+  head "https://github.com/qbittorrent/qBittorrent.git", branch: "master"
+
+  depends_on "cmake" => :build
+  depends_on "ninja" => :build
+
+  depends_on "libtorrent-rasterbar"
+  depends_on "openssl"
+  depends_on "qt"
+
+  def install
+    args = std_cmake_args + %w[
+      -S .
+      -B build
+      -G Ninja
+      -D VERBOSE_CONFIGURE=ON
+      -D QT6=ON
+    ]
+    system "cmake", *args
+    system "cmake", "--build", "build"
+    system "cmake", "--install", "build"
+
+    qtpp = `#{Formula["qt"].bin}/qtpaths --plugin-dir`.chomp
+    system "/usr/libexec/PlistBuddy",
+      "-c", "Add :LSEnvironment:QT_PLUGIN_PATH string \"#{qtpp}\"",
+      "#{prefix}/qbittorrent.app/Contents/Info.plist"
+  end
+
+  test do
+    system "false"
+  end
+end
