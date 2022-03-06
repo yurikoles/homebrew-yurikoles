40b5b094f4df0526f654dd78208931c593b76fa2
Yurii Kolesnykov <root@yurikoles.com>
2022-03-06 03:07:43 +0200

    qbittorrent
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Formula/qbittorrent-qt6.rb
+++ Formula/qbittorrent-qt6.rb
@@ -1,4 +1,4 @@
-class Qbittorrent < Formula
+class QbittorrentQt6 < Formula
   desc "BitTorrent client"
   homepage "https://www.qbittorrent.org/"
   url "https://downloads.sourceforge.net/project/qbittorrent/qbittorrent/qbittorrent-4.4.0/qbittorrent-4.4.0.tar.xz"
@@ -11,23 +11,25 @@ class Qbittorrent < Formula
 
   depends_on "libtorrent-rasterbar"
   depends_on "openssl"
-  depends_on "qt@5"
+  depends_on "qt"
 
   def install
     args = std_cmake_args + %w[
       -S .
       -B build
       -G Ninja
+      -D QT6=ON
     ]
+
     system "cmake", *args
     system "cmake", "--build", "build"
     system "cmake", "--install", "build"
 
     prefix.install "build/install_manifest.txt"
     # Extract Qt plugin path
-    qtpp = `#{Formula["qt@5"].bin}/qtpaths --plugin-dir`.chomp
+    qtpp = `#{Formula["qt"].bin}/qtpaths --plugin-dir`.chomp
     system "/usr/libexec/PlistBuddy",
-      "-c", "Add :LSEnvironment:QT_PLUGIN_PATH string \"#{qtpp}\:#{HOMEBREW_PREFIX}/lib/qt5/plugins\"",
+      "-c", "Add :LSEnvironment:QT_PLUGIN_PATH string \"#{qtpp}\"",
       "#{prefix}/qbittorrent.app/Contents/Info.plist"
   end
 
