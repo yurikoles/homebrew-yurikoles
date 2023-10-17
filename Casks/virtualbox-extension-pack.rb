8fc0072d8cdff32d85e80631d3cda124bb8b3dca
Yurii Kolesnykov <root@yurikoles.com>
2023-10-17 20:31:37 +0300

    extract and update deprecated virtualbox-extension-pack
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

---
+++ Casks/virtualbox-extension-pack.rb
@@ -0,0 +1,45 @@
+cask "virtualbox-extension-pack" do
+  version "7.0.12"
+  sha256 "dbf7ce39e5c021d420fc6b2045b084a68fc5172937192bd70c3207efa786278d"
+
+  url "https://download.virtualbox.org/virtualbox/#{version}/Oracle_VM_VirtualBox_Extension_Pack-#{version}.vbox-extpack"
+  name "Oracle VirtualBox Extension Pack"
+  desc "Extend the functionality of VirtualBox"
+  homepage "https://www.virtualbox.org/"
+
+  livecheck do
+    url "https://download.virtualbox.org/virtualbox/LATEST.TXT"
+    regex(/(\d+(?:\.\d+)+)/i)
+  end
+
+  conflicts_with cask: "virtualbox-extension-pack-beta"
+  depends_on cask: "virtualbox"
+  container type: :naked
+
+  stage_only true
+
+  postflight do
+    system_command "/usr/local/bin/VBoxManage",
+                   args:  [
+                     "extpack", "install",
+                     "--replace", "#{staged_path}/Oracle_VM_VirtualBox_Extension_Pack-#{version}.vbox-extpack"
+                   ],
+                   input: "y",
+                   sudo:  true
+  end
+
+  uninstall_postflight do
+    next unless File.exist?("/usr/local/bin/VBoxManage")
+
+    system_command "/usr/local/bin/VBoxManage",
+                   args: [
+                     "extpack", "uninstall",
+                     "Oracle VM VirtualBox Extension Pack"
+                   ],
+                   sudo: true
+  end
+
+  caveats do
+    license "https://www.virtualbox.org/wiki/VirtualBox_PUEL"
+  end
+end
