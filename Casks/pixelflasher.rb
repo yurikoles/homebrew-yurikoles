d381e2351f7d87801ceb17976f14a79026a5ac35
Yurii Kolesnykov <root@yurikoles.com>
2024-07-18 09:20:54 +0100

    pixelflasher 7.1.1.1 (#19)

--- Casks/pixelflasher.rb
+++ Casks/pixelflasher.rb
@@ -1,6 +1,6 @@
 cask "pixelflasher" do
-  version "7.1.0.0"
-  sha256 "8258b8a37c862ece71349c81a92ce3994f4ebde1144673b7599e8544400ee27e"
+  version "7.1.1.1"
+  sha256 "35e6889a3e0e76a1b1a04a0c9ff262128828d472e8a23f95917f7ae73b4c4c6b"
 
   url "https://github.com/badabing2005/PixelFlasher/releases/download/v#{version}/PixelFlasher.dmg"
   name "PixelFlasher"
