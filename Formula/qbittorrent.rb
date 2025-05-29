09a2e5d6f501d78bd53d62025a329a6e2014e58e
Yurii Kolesnykov <root@yurikoles.com>
2025-05-29 03:57:16 +0100

    Update qbittorrent.rb

--- Formula/qbittorrent.rb
+++ Formula/qbittorrent.rb
@@ -10,28 +10,16 @@ class Qbittorrent < Formula
   depends_on "ninja" => :build
 
   depends_on "libtorrent-rasterbar"
-  depends_on "openssl@1.1"
-  depends_on "qt" => :recommended
-  depends_on "molten-vk" => :optional
-  depends_on "qt@5" => :optional
+  depends_on "openssl"
+  depends_on "qt"
 
   def install
     args = %w[
       -S .
       -B build
       -G Ninja
+      -D QT6=ON
     ]
-
-    qt_dep = ""
-    if build.with? "qt"
-      qt_dep = "qt"
-      args << "-D QT6=ON"
-    elsif build.with? "qt@5"
-      qt_dep = "qt@5"
-      args << "-D QT6=OFF"
-    end
-
-    args << "-D Vulkan_INCLUDE_DIR=#{Formula["molten-vk"].opt_include}" if build.with? "molten-vk"
 
     system "cmake", *std_cmake_args, *args
     system "cmake", "--build", "build"
@@ -39,7 +27,7 @@ class Qbittorrent < Formula
 
     prefix.install "build/install_manifest.txt"
     # Extract Qt plugin path
-    qtpp = `#{Formula[qt_dep].bin}/qtpaths --plugin-dir`.chomp
+    qtpp = `#{Formula["qt"].bin}/qtpaths --plugin-dir`.chomp
     system "/usr/libexec/PlistBuddy",
       "-c", "Add :LSEnvironment:QT_PLUGIN_PATH string \"#{qtpp}\"",
       "#{prefix}/qbittorrent.app/Contents/Info.plist"
