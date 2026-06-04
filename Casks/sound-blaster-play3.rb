21a01ecd77081ea8256b7a13a6470f24f81d6af6
Yurii Kolesnykov <root@yurikoles.com>
2026-06-04 17:13:24 +0100

    sound-blaster-play3: depends on catalina
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/sound-blaster-play3.rb
+++ Casks/sound-blaster-play3.rb
@@ -1,41 +1,24 @@
 cask "sound-blaster-play3" do
   # NOTE: "3" is not a version number, but an intrinsic part of the product name
-  on_mojave :or_older do
-    version "2.00.03"
-    sha256 "0550328b70fc214fcffc418fba3e5140c853e827bc92227a73864d62ba5e8415"
+  version "2.01.02"
+  sha256 "3dbbdc627f5ee72f87403dc471e6718c258594e0fb0b208d900a5f174ce02915"
 
-    url "https://files.creative.com/manualdn/Drivers/AVP/13795/0x3F213863/SBP3_MAC_L13_#{version.dots_to_underscores}.dmg.zip"
-
-    livecheck do
-      url "https://support.creative.com/Products/ProductDetails.aspx?catID=1&subCatID=1258&prodID=23033"
-      regex(/SBP3[._-]MAC[._-]L13[._-]?v?(\d+(?:[._]\d+)+)\.dmg\.zip\s/i)
-      strategy :page_match do |page, regex|
-        page.scan(regex).map { |match| match[0].tr("_", ".") }
-      end
-    end
-
-    pkg "Install.pkg"
-  end
-  on_catalina :or_newer do
-    version "2.01.02"
-    sha256 "3dbbdc627f5ee72f87403dc471e6718c258594e0fb0b208d900a5f174ce02915"
-
-    url "https://files.creative.com/manualdn/Drivers/100289/MqrOwTo9a7/SBP3_MAC_L13_#{version.dots_to_underscores}.dmg"
-
-    livecheck do
-      url "https://support.creative.com/Products/ProductDetails.aspx?catID=1&subCatID=1258&prodID=23033"
-      regex(/SBP3[._-]MAC[._-]L13[._-]?v?(\d+(?:[._]\d+)+)\.dmg\s/i)
-      strategy :page_match do |page, regex|
-        page.scan(regex).map { |match| match[0].tr("_", ".") }
-      end
-    end
-
-    pkg "INSTALL.pkg"
-  end
-
+  url "https://files.creative.com/manualdn/Drivers/100289/MqrOwTo9a7/SBP3_MAC_L13_#{version.dots_to_underscores}.dmg"
   name "Sound Blaster PLAY! 3"
   desc "Driver and control panel for the Sound Blaster PLAY! 3 USB DAC and Amp"
   homepage "https://us.creative.com/p/sound-cards/sound-blaster-play-3"
+
+  livecheck do
+    url "https://support.creative.com/Products/ProductDetails.aspx?catID=1&subCatID=1258&prodID=23033"
+    regex(/SBP3[._-]MAC[._-]L13[._-]?v?(\d+(?:[._]\d+)+)\.dmg\s/i)
+    strategy :page_match do |page, regex|
+      page.scan(regex).map { |match| match[0].tr("_", ".") }
+    end
+  end
+
+  depends_on macos: :catalina
+
+  pkg "INSTALL.pkg"
 
   uninstall launchctl: "com.creative.trustudiopc",
             pkgutil:   [
