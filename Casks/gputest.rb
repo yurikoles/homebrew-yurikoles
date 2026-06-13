3cb1c5f7800b31912142ea7bb95f9676b72e40eb
Yurii Kolesnykov <root@yurikoles.com>
2026-06-13 20:52:01 +0100

    gputest: lint
    Signed-off-by: Yurii Kolesnykov <root@yurikoles.com>

--- Casks/gputest.rb
+++ Casks/gputest.rb
@@ -1,12 +1,22 @@
 cask "gputest" do
-  version "0.7.0"
+  version "0.7.0,391"
   sha256 "b02cd3c381c751bdc080e7bd004267d219d6112f5ecf9ab1db94196ccc85d7c0"
 
-  url "https://www.geeks3d.com/dl/get/391"
+  url "https://www.geeks3d.com/dl/get/#{version.csv.second}"
   name "GpuTest"
   desc "Cross Platform OpenGL Benchmark"
   homepage "https://www.geeks3d.com/GpuTest/"
 
+  livecheck do
+    skip "No version information available"
+  end
+
+  depends_on :macos
+
   app "GpuTest.app"
   app "GpuTest_GUI.app"
+
+  caveats do
+    requires_rosetta
+  end
 end
